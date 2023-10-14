56f085acafa54eb5c80fd8db84a8457f8fdb2c9e
saurabhkawatra <saurabh.kawatra1@gmail.com>
2023-10-15 00:19:49 +0530

    first commit

---
+++ src/main/java/com/pokemon/app/PokemonSearchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pokemon.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonSearchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonSearchBackendApplication.class, args);
+	}
+
+}
